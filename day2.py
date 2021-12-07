7e9a3aa408ffe5e6245a4cd5c0603823b0466caa
Marcus Williams <marcus.williams@audinate.com>
2021-12-07 19:41:02 +0000

    Day 2

---
+++ day2.py
@@ -0,0 +1,25 @@
+import cmd
+
+class Submarine(cmd.Cmd):
+    depth = 0
+    horizontal = 0
+    use_rawinput = False
+
+    def do_forward(self, args):
+        self.horizontal += int(args[0])
+
+    def do_down(self, args):
+        self.depth += int(args[0])
+
+    def do_up(self, args):
+        self.depth -= int(args[0])
+
+if __name__ == '__main__':
+    sub = Submarine()
+    with open("input_day2") as f:
+        for line in f.readlines():
+            sub.onecmd(line)
+
+    print(sub.depth, sub.horizontal)
+    print(sub.depth * sub.horizontal)
+
