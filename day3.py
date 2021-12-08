c80ba7bfb3d0edc06d4bbfbb62822423362b10da
Marcus Williams <marcus.williams@audinate.com>
2021-12-08 11:15:24 +0000

    ints or strings procrastination

--- day3.py
+++ day3.py
@@ -8,11 +8,11 @@ for d in data:
     for i, f in enumerate(list(d[:-1])):
         sums[str(i)] += int(f)
 
-gamma = [(len(data)-a)<a and "1" or "0" for (_, a) in sums.items()]
-epsilon = [(len(data)-a)>a and "1" or "0" for (_, a) in sums.items()]
+gamma = [(len(data)-a)<a and 1 or 0 for (_, a) in sums.items()]
+epsilon = [int(not g) for g in gamma]
 
-gamma = int("".join(gamma), 2)
-epsilon = int("".join(epsilon), 2)
+gamma = int("".join(map(str, gamma)), 2)
+epsilon = int("".join(map(str, epsilon)), 2)
 
 print(gamma*epsilon)
 
